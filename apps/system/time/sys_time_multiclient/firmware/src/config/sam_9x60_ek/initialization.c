98a828c34b7b62ab101f176c3667c70e0b869124
Syam Nair <syam.nair@microchip.com>
2021-02-02 16:34:02 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_sam_9x60_ek
    MH3-50128
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x60_ek/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x60_ek/initialization.c
@@ -203,6 +203,7 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
     STDIO_BufferModeSet();
@@ -218,7 +219,7 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
