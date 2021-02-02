b0530a3e69a7d56ac038c7740e41a1438811df8e
Syam Nair <syam.nair@microchip.com>
2021-02-02 16:35:14 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_sam_9x60_ek_iar
    MH3-50129
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x60_ek_iar/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x60_ek_iar/initialization.c
@@ -184,6 +184,7 @@ static void SYSC_Disable( void )
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
 
@@ -197,7 +198,7 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
