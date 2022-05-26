e78c96df8a6c3b439474391286ad0d4c5975deae
Syam Nair <syam.nair@microchip.com>
2022-05-26 08:46:29 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_9x60_ek_freertos
    MH3-64804
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_9x60_ek_freertos/definitions.h
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_9x60_ek_freertos/definitions.h
@@ -57,11 +57,11 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "peripheral/flexcom/twi/master/plib_flexcom0_twi_master.h"
-#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "driver/i2c/drv_i2c.h"
 #include "app_i2c_eeprom.h"
 
 
