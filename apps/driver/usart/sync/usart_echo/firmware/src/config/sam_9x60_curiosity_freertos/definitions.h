7619e2b264a45e46b6d8b15d6f6618b232459089
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-11-06 16:00:30 +0530

    [DRV_] Regenerated demo for driver__usart__sync__usart_echo__firmware__sam_9x60_curiosity_freertos.X
    MH3-103488

--- apps/driver/usart/sync/usart_echo/firmware/src/config/sam_9x60_curiosity_freertos/definitions.h
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/sam_9x60_curiosity_freertos/definitions.h
@@ -55,9 +55,9 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
 #include "peripheral/dbgu/plib_dbgu.h"
+#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "system/dma/sys_dma.h"
@@ -76,13 +76,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "SAM9X60D1G"
-#define DEVICE_ARCH			 "ARM926EJ-S"
-#define DEVICE_FAMILY		 "SAM9"
-#define DEVICE_SERIES		 "SAM9X"
+#define DEVICE_NAME          "SAM9X60D1G"
+#define DEVICE_ARCH          "ARM926EJ-S"
+#define DEVICE_FAMILY        "SAM9"
+#define DEVICE_SERIES        "SAM9X6"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 600000000
+#define CPU_CLOCK_FREQUENCY 600000000U
 
 // *****************************************************************************
 // *****************************************************************************
