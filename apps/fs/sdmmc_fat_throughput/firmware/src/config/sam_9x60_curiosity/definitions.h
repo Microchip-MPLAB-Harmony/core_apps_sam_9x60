b82330209f28fd4493188b664e6bda0afddd6a39
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-11-06 15:57:59 +0530

    [SYS_FS] Regenerated demo for fs__sdmmc_fat_throughput__firmware__sam_9x60_curiosity.X
    MH3-103498

--- apps/fs/sdmmc_fat_throughput/firmware/src/config/sam_9x60_curiosity/definitions.h
+++ apps/fs/sdmmc_fat_throughput/firmware/src/config/sam_9x60_curiosity/definitions.h
@@ -49,13 +49,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "driver/sdmmc/drv_sdmmc.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
 #include "system/fs/fat_fs/file_system/ff.h"
 #include "system/fs/fat_fs/file_system/ffconf.h"
 #include "system/fs/fat_fs/hardware_access/diskio.h"
+#include "driver/sdmmc/drv_sdmmc.h"
 #include "peripheral/mmu/plib_mmu.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
@@ -82,13 +82,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "SAM9X60D1G"
-#define DEVICE_ARCH			 "ARM926EJ-S"
-#define DEVICE_FAMILY		 "SAM9"
-#define DEVICE_SERIES		 "SAM9X"
+#define DEVICE_NAME          "SAM9X60D1G"
+#define DEVICE_ARCH          "ARM926EJ-S"
+#define DEVICE_FAMILY        "SAM9"
+#define DEVICE_SERIES        "SAM9X6"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 600000000
+#define CPU_CLOCK_FREQUENCY 600000000U
 
 // *****************************************************************************
 // *****************************************************************************
