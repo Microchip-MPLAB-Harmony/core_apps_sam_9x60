e61210b860ca5583991e0074e7ab9ccd3027e70b
Syam Nair <Syam.Nair@microchip.com>
2025-11-25 11:13:17 +0000

    Regen all applications to fix component yamls
    MH3C-397

--- apps/driver/usart/sync/usart_multi_instance/firmware/src/config/sam_9x60_curiosity_freertos/peripheral/flexcom/usart/plib_flexcom7_usart.c
+++ apps/driver/usart/sync/usart_multi_instance/firmware/src/config/sam_9x60_curiosity_freertos/peripheral/flexcom/usart/plib_flexcom7_usart.c
@@ -66,9 +66,9 @@
 // Section: FLEXCOM7 USART Implementation
 // *****************************************************************************
 // *****************************************************************************
-volatile static FLEXCOM_USART_OBJECT flexcom7UsartObj;
+static volatile FLEXCOM_USART_OBJECT flexcom7UsartObj;
 
-void static FLEXCOM7_USART_ErrorClear( void )
+static void FLEXCOM7_USART_ErrorClear( void )
 {
     if ((FLEXCOM7_REGS->FLEX_US_CSR & (FLEX_US_CSR_OVRE_Msk | FLEX_US_CSR_FRAME_Msk | FLEX_US_CSR_PARE_Msk)) != 0U)
     {
@@ -91,7 +91,7 @@ void static FLEXCOM7_USART_ErrorClear( void )
 }
 
 
-void static __attribute__((used)) FLEXCOM7_USART_ISR_RX_Handler( void )
+static void __attribute__((used)) FLEXCOM7_USART_ISR_RX_Handler( void )
 {
     uint32_t rxPending = 0;
     uint32_t rxThreshold = 0;
@@ -149,7 +149,7 @@ void static __attribute__((used)) FLEXCOM7_USART_ISR_RX_Handler( void )
     }
 }
 
-void static __attribute__((used)) FLEXCOM7_USART_ISR_TX_Handler( void )
+static void __attribute__((used)) FLEXCOM7_USART_ISR_TX_Handler( void )
 {
     if(flexcom7UsartObj.txBusyStatus == true)
     {
@@ -371,7 +371,7 @@ bool FLEXCOM7_USART_SerialSetup( FLEXCOM_USART_SERIAL_SETUP *setup, uint32_t src
         else
         {
             /* Requested baud can be generated with either with 8x oversampling or with 16x oversampling. Select valid one. */
-            if ((cd1 > 0U )&& (cd1 <= 65535U))
+            if ((cd1 > 0U) && (cd1 <= 65535U))
             {
                 cd0 = cd1;
                 fp0 = fp1;
@@ -542,4 +542,6 @@ bool FLEXCOM7_USART_TransmitComplete( void )
 {
     return ((FLEXCOM7_REGS->FLEX_US_CSR & FLEX_US_CSR_TXEMPTY_Msk) != 0U);
 }
+
+
 
