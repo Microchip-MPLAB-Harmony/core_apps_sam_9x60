c2a5b8970ecb61acba1faa719d2986323bd9eeb7
Pranjal Das <pranjal.das@microchip.com>
2026-04-30 22:08:02 +0530

    [DRV_SMC_FLASH] Regenerated demo for apps__driver__smc_flash__nand_flash__nand_flash_read_write__firmware__sam_9x60_curiosity.X
    MH3C-854

--- apps/driver/smc_flash/nand_flash/nand_flash_read_write/firmware/src/packs/SAM9X60D1G_DFP/instance/dbgu.h
+++ apps/driver/smc_flash/nand_flash/nand_flash_read_write/firmware/src/packs/SAM9X60D1G_DFP/instance/dbgu.h
@@ -1,7 +1,9 @@
 /*
  * Instance header file for ATSAM9X60D6K
  *
- * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2026 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,15 +19,15 @@
  *
  */
 
-/* file generated from device description file (ATDF) version 2024-01-15T14:39:57Z */
+/* file generated from device description file (ATDF) version 2026-03-27T10:04:21Z */
 #ifndef _SAM9X6_DBGU_INSTANCE_
 #define _SAM9X6_DBGU_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for DBGU peripheral ========== */
-#define DBGU_CLOCK_ID                            (47)       
-#define DBGU_DMAC_ID_RX                          (29)       
-#define DBGU_DMAC_ID_TX                          (28)       
-#define DBGU_INSTANCE_ID                         (47)       
+#define DBGU_CLOCK_ID                            (47)
+#define DBGU_DMAC_ID_RX                          (29)
+#define DBGU_DMAC_ID_TX                          (28)
+#define DBGU_INSTANCE_ID                         (47)
 
 #endif /* _SAM9X6_DBGU_INSTANCE_ */
