e6c72f427db935bb0da86f096939806ebf35b861
Pranjal Das <pranjal.das@microchip.com>
2026-04-30 22:18:40 +0530

    [DRV_SPI] Regenerated demo for apps__driver__spi__async__spi_self_loopback_multi_client__firmware__sam_9x60_curiosity_freertos.X
    MH3C-854

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_9x60_curiosity_freertos/driver/spi/src/drv_spi.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_9x60_curiosity_freertos/driver/spi/src/drv_spi.c
@@ -249,8 +249,8 @@ static DRV_SPI_TRANSFER_OBJ* lDRV_SPI_FreeTransferObjGet(DRV_SPI_CLIENT_OBJ* cli
     }
     return NULL;
 }
-/* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -
-   H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+/* MISRA C-2023 Rule 11.3, 11.8 deviated below. Deviation record ID -
+   H3_MISRAC_2023_R_11_3_DR_1 & H3_MISRAC_2023_R_11_8_DR_1*/
 
 static bool lDRV_SPI_TransferObjAddToList(
     DRV_SPI_OBJ* dObj,
