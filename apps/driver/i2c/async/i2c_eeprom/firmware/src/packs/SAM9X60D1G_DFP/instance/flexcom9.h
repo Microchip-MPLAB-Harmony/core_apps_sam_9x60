09b234a9bea72400c02a106066f698dc851edd1a
Pranjal Das <pranjal.das@microchip.com>
2026-04-30 22:18:32 +0530

    [DRV_I2C] Regenerated demo for apps__driver__i2c__async__i2c_eeprom__firmware__sam_9x60_curiosity_freertos.X
    MH3C-854

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/packs/SAM9X60D1G_DFP/instance/flexcom9.h
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/packs/SAM9X60D1G_DFP/instance/flexcom9.h
@@ -1,7 +1,9 @@
 /*
  * Instance header file for ATSAM9X60D6K
  *
- * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2026 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,18 +19,18 @@
  *
  */
 
-/* file generated from device description file (ATDF) version 2024-01-15T14:39:57Z */
+/* file generated from device description file (ATDF) version 2026-03-27T10:04:21Z */
 #ifndef _SAM9X6_FLEXCOM9_INSTANCE_
 #define _SAM9X6_FLEXCOM9_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for FLEXCOM9 peripheral ========== */
-#define FLEXCOM9_CLOCK_ID                        (15)       
-#define FLEXCOM9_DMAC_ID_RX                      (19)       
-#define FLEXCOM9_DMAC_ID_TX                      (18)       
-#define FLEXCOM9_INSTANCE_ID                     (15)       
-#define FLEXCOM9_SPI_FIFO_SIZE                   (16)       
-#define FLEXCOM9_TWI_FIFO_SIZE                   (16)       
-#define FLEXCOM9_USART_FIFO_SIZE                 (16)       
+#define FLEXCOM9_CLOCK_ID                        (15)
+#define FLEXCOM9_DMAC_ID_RX                      (19)
+#define FLEXCOM9_DMAC_ID_TX                      (18)
+#define FLEXCOM9_INSTANCE_ID                     (15)
+#define FLEXCOM9_SPI_FIFO_SIZE                   (16)
+#define FLEXCOM9_TWI_FIFO_SIZE                   (16)
+#define FLEXCOM9_USART_FIFO_SIZE                 (16)
 
 #endif /* _SAM9X6_FLEXCOM9_INSTANCE_ */
